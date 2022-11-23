c93dde47ce1bd6342563a7f95285e234141b5ec2
Dan-Sutton <dewasutton@gmail.com>
2022-11-23 23:07:29 +0000

    source

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
       <Navbar />
       <div className="container">
         <Routes>
-          <Route path="/" element={<Projects />} />
+          <Route path="/" element={<Home />} />
           <Route path={"/projects"} element={<Projects />} />
           <Route path={"/contact"} element={<Contact />} />
         </Routes>
