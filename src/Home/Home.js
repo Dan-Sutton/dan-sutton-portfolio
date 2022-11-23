0e4951a49ef43ad9297135e03a2c65e718defdaa
Dan-Sutton <dewasutton@gmail.com>
2022-11-23 20:49:03 +0000

    socials

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,8 +1,15 @@
 import React from "react";
 import profileImage from "../assets/profileImage.jpeg";
 import "./home.css";
+import resume from "../assets/resume.pdf";
+import ghLogo from "../assets/ghLogo.png";
+import linkedinLogo from "../assets/linkedinLogo.png";
 
 function Home(props) {
+  function openResume() {
+    window.open(resume);
+  }
+
   return (
     <>
       <div className="home-content-row">
@@ -11,8 +18,13 @@ function Home(props) {
           <h1>Hi, I'm Dan!</h1>
           <p>I'm a Software Engineer based in Shropshire, UK. </p>
           <div className="home-buttons">
-            <button className="resume-button">RESUME</button>
-            <button className="projects-button">PROJECTS</button>
+            <button className="resume-button" onClick={() => openResume()}>
+              RESUME
+            </button>
+            <div className="logos">
+              <img src={ghLogo} className="ghlogo"></img>
+              <img src={linkedinLogo} className="linkedinlogo"></img>
+            </div>
           </div>
         </div>
       </div>
