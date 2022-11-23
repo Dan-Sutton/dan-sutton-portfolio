006333a93ef793c593b2b457dbf5b470228624db
Dan-Sutton <dewasutton@gmail.com>
2022-11-23 22:06:34 +0000

    typing effect

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -4,6 +4,7 @@ import "./home.css";
 import resume from "../assets/resume.pdf";
 import ghLogo from "../assets/ghLogo.png";
 import linkedinLogo from "../assets/linkedinLogo.png";
+import Typewriter from "typewriter-effect";
 
 function Home(props) {
   function openResume() {
@@ -15,15 +16,44 @@ function Home(props) {
       <div className="home-content-row">
         <img src={profileImage} className="profile-image"></img>
         <div className="content-column">
-          <h1>Hi, I'm Dan!</h1>
+          <div className="home-title">
+            <Typewriter
+              options={{
+                strings: ["Hi, I'm Dan!"],
+                autoStart: true,
+                loop: true,
+              }}
+            />
+          </div>
+
           <p>I'm a Software Engineer based in Shropshire, UK. </p>
           <div className="home-buttons">
             <button className="resume-button" onClick={() => openResume()}>
               RESUME
             </button>
             <div className="logos">
-              <img src={ghLogo} className="ghlogo"></img>
-              <img src={linkedinLogo} className="linkedinlogo"></img>
+              <img
+                src={ghLogo}
+                className="ghlogo"
+                onClick={() =>
+                  window.open(
+                    "https://github.com/Dan-Sutton",
+                    "_blank",
+                    "noopener,noreferrer"
+                  )
+                }
+              ></img>
+              <img
+                onClick={() =>
+                  window.open(
+                    "https://www.linkedin.com/in/dewa-sutton/",
+                    "_blank",
+                    "noopener,noreferrer"
+                  )
+                }
+                src={linkedinLogo}
+                className="linkedinlogo"
+              ></img>
             </div>
           </div>
         </div>
