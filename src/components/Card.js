3c38ace5f1b375d88d10592ecd2066cf41b4880a
Dan-Sutton <dewasutton@gmail.com>
2022-11-24 16:45:00 +0000

    centered card

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,7 +2,14 @@ import React from "react";
 import "./card.css";
 
 function Card(props) {
-  return <div className="card"></div>;
+  return (
+    <div className="cards">
+      <div className="big-card"></div>
+      <div className="big-card"></div>
+      <div className="big-card"></div>
+      <div className="big-card"></div>
+    </div>
+  );
 }
 
 export default Card;
