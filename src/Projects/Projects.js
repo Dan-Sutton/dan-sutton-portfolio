7e7d12a367cccb3a5dd0fc9a03bf1a5481d882e3
Dan-Sutton <dewasutton@gmail.com>
2022-11-24 17:26:20 +0000

    titles

--- src/Projects/Projects.js
+++ src/Projects/Projects.js
@@ -5,7 +5,13 @@ import Card from "../components/Card";
 function Projects() {
   return (
     <div id="cards">
-      <Card />
+      <Card title={"Cookr"} subtitle={"Random recipe finder"} />
+      <Card
+        title={"Band Website"}
+        subtitle={"A Band showcasing site, where bookings can be made"}
+      />
+      <Card title={"Last Minute Band"} subtitle={"Find and book local bands"} />
+      <Card title={"Play it"} subtitle={"Make song requests"} />
     </div>
   );
 }
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,13 +1,16 @@
 import React from "react";
 import "./card.css";
 
-function Card(props) {
+function Card({ title, subtitle }) {
   return (
     <div className="cards">
-      <div className="big-card"></div>
-      <div className="big-card"></div>
-      <div className="big-card"></div>
-      <div className="big-card"></div>
+      <div class="card">
+        <div class="card-content">
+          <h3 class="card-title">{title}</h3>
+          <h4 class="card-subtitle">{subtitle}</h4>
+        </div>
+        <i class="fa-solid fa-hat-witch card-icon"></i>
+      </div>
     </div>
   );
 }
