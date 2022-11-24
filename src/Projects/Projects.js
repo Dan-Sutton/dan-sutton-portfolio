ebe95faa7bd0552f78323261734363ee9b20a256
Dan-Sutton <dewasutton@gmail.com>
2022-11-24 16:32:01 +0000

    more subtle color

--- src/Projects/Projects.js
+++ src/Projects/Projects.js
@@ -2,39 +2,12 @@ import React from "react";
 import "./projects.css";
 
 function Projects() {
-  const handleOnMouseMove = (e) => {
-    const { currentTarget: target } = e;
-
-    const rect = target.getBoundingClientRect(),
-      x = e.clientX - rect.left,
-      y = e.clientY - rect.top;
-
-    target.style.setProperty("--mouse-x", `${x}px`);
-    target.style.setProperty("--mouse-y", `${y}px`);
-  };
-
-  for (const card of document.querySelectorAll(".card")) {
-    card.onmousemove = (e) => handleOnMouseMove(e);
-  }
-
   return (
     <div id="cards">
-      <div className="card">
-        <div className="card-border"></div>
-        <div className="card-content"></div>
-      </div>
-      <div className="card">
-        <div className="card-border"></div>
-        <div className="card-content"></div>
-      </div>
-      <div className="card">
-        <div className="card-border"></div>
-        <div className="card-content"></div>
-      </div>
-      <div className="card">
-        <div className="card-border"></div>
-        <div className="card-content"></div>
-      </div>
+      <div className="card"></div>
+      <div className="card"></div>
+      <div className="card"></div>
+      <div className="card"></div>
     </div>
   );
 }
