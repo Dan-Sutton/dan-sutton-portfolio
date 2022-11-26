762d67667667c51cd6300a01992c869b85c81be9
Dan-Sutton <dewasutton@gmail.com>
2022-11-26 17:48:33 +0000

    coookr modal

--- src/assets/SeedData/modalInfo.js
+++ src/assets/SeedData/modalInfo.js
@@ -2,9 +2,11 @@ export const modalInfo = {
   cookr: {
     title: "Cookr",
     description:
-      "With the creation of this app, I aimed to solve the daily issue of what to have at meal times. I was inspired by the quick and simple useage of Tinder, which allows a user to have a quick overview of information, allowing them to quickly decide 'yes', or 'no'.",
+      "With the creation of this app, I aimed to solve the daily issue of what to have at meal times. I was inspired by the quick and simple useage of Tinder, which gives the user a quick overview of information, allowing them to quickly decide 'yes', or 'no'.",
     video:
       "https://res.cloudinary.com/dsdpn6tzq/video/upload/v1669483497/Website/cookr_vid_ifq85d.mov",
+    github: "",
+    link: "https://dan-sutton.github.io/cookr/",
   },
   bandSite: "Band Site",
 };
